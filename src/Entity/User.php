f9ab4c6a903603c0236c8d8e18261308eb65994d
Steven <sivakaran19@hotmail.fr>
2022-12-02 16:37:12 +0100

    Add Login Form and base of Admin page

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -149,6 +149,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getFullName(): ?string
+    {
+        return $this->firstname . " " . $this->name;
+    }
+
     public function getSexe(): ?string
     {
         return $this->sexe;
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -39,6 +39,23 @@ class ArticleRepository extends ServiceEntityRepository
         }
     }
 
+     /**
+    * @return Article[] Returns an array of Article objects
+    */
+   public function findByCreatedDate(int $author = null): array
+   {
+        $qb = $this->createQueryBuilder('article');
+
+        if($author) {
+            $qb->andWhere('article.author = :author')
+            ->setParameter('author', $author);
+        }
+        $qb->orderBy('a.editedAt', 'DESC')
+        ->setMaxResults(3);
+
+        return $qb->getQuery()->getResult();
+   }
+
 //    /**
 //     * @return Article[] Returns an array of Article objects
 //     */
